3772198045c69119b17bac261da33bf8f5a42a7c
PietroCappelletti <pietrocappelletti.dev@gmail.com>
2024-09-19 16:27:09 +0100

    implement basepath

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /**
  * @type {import('next').NextConfig}
  */
+
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
+    basePath: isProd ? '/pietrocappelletti-blog' : '',
     output: 'export',
     // Optional: Change the output directory `out` -> `dist`
     distDir: 'dist',
